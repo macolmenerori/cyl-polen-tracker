1a60aa6e89464b9ca707cd6f629ed1bee75f761b
macolmenerori <miguelangelcolmenero@gmail.com>
2025-08-12 11:12:21 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './i18n';
+
+import { App } from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <Suspense fallback={<p>Loading...</p>}>
+    <App />
+  </Suspense>
+);
