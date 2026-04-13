ce84211b8a685289181ddfdee480bac54622b86f
macolmenerori <miguelangelcolmenero@gmail.com>
2026-04-13 12:02:01 +0200

    fix: Remove temporary CSS types

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,4 +1,3 @@
 // CSS-only side-effect imports from packages that ship no .d.ts stubs.
 // Required since TypeScript 6 enables noUncheckedSideEffectImports by default.
-declare module '@macolmenerori/component-library/theme-switch-css';
 declare module 'mapbox-gl/dist/mapbox-gl.css';
